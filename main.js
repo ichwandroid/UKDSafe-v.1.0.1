a49e1cf32fa0df70ce6d9c23cdf89915b16e591d
ichwanap <ichwan.laruku@gmail.com>
2025-04-24 22:45:38 +0700

    feat: initialize project with Electron and add preload script
    - Created package.json for project configuration and dependencies
    - Added Electron as a development dependency
    - Implemented preload.js to expose API for renderer process

---
+++ main.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const path = require('path');
+
+let splash, mainWindow;
+
+function createSplash() {
+  splash = new BrowserWindow({
+    width: 400,
+    height: 300,
+    resizable: false,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  splash.loadFile('assets/splash.html');
+
+  splash.webContents.on('did-fail-load', () => {
+    console.error('Failed to load splash screen');
+  });
+}
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    kiosk: true,
+    frame: false,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadURL("https://youtube.com");
+
+  // Cegah navigasi keluar
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    if (url !== mainWindow.webContents.getURL()) e.preventDefault();
+  });
+
+  mainWindow.webContents.on('new-window', e => e.preventDefault());
+
+  mainWindow.webContents.on('did-fail-load', () => {
+    console.error('Failed to load main window');
+  });
+
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.setFullScreen(true);
+
+  // Shortcut keluar dari aplikasi
+  globalShortcut.register('Control+Shift+Q', () => {
+    app.quit();
+  });
+}
+
+app.whenReady().then(() => {
+  createSplash();
+});
+
+// Handle login dari splash
+ipcMain.on('login-success', () => {
+  console.log('Login berhasil, membuka main window...');
+  splash.close(); // Tutup splash screen
+  createMainWindow(); // Buat dan tampilkan mainWindow setelah login berhasil
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
